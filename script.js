10b17d99f2a710448bf3b8901ee66361a2a64c7a
sudip <paudelsudip134@gmail.com>
2023-09-25 18:52:40 +0545

    code formated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var Typed =new Typed(".animated-text",{
-    strings:["Nostalgia","Thrilling","Addiction","Emotion","Everything"],
-    typeSpeed:50,
-    backSpeed:20,
-    loop:true,
+var Typed = new Typed(".animated-text", {
+    strings: ["Nostalgia", "Thrilling", "Addiction", "Emotion", "Everything"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    loop: true,
 });
